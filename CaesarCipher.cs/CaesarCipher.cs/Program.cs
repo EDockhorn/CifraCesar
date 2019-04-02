7c9758fbc32c14fdfc16227f0bdfe01158acea9b
Johnny Erick Dockhorn <johnnyerick111@hotmail.com>
2019-04-02 12:24:39 -0300

    QUARTO COMMIT AEEEE

--- CaesarCipher.cs/CaesarCipher.cs/Program.cs
+++ CaesarCipher.cs/CaesarCipher.cs/Program.cs
@@ -10,7 +10,7 @@ namespace CaesarCipher.cs
     {
         static void Main(string[] args)
         {
-            //TESTE COMMIT
+            
             EncriptDecript encrypt = new EncriptDecript();
             Console.WriteLine("ENTER WITH YOURE PASSCODE: ");
             encrypt.commonword = Console.ReadLine();
